eb8162d57c1430e1915fd24318bc207ee7dcc5af
ft4bhi <psabhiram100@gmail.com>
2026-07-21 15:36:12 +0530

    front end added

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,12 +1,21 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Note
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = serializers.User
+        model = User
         fields = ['id', 'username', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            return user
+    def create(self, validated_data):
+        print("Creating user with validated data")  # Debugging line
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at']
+        extra_kwargs = {'author': {'read_only': True}}
+    
